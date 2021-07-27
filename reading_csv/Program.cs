ee26bc26494c78e70653e45bf44ca5967bb44bb1
VedantSonje <vedant.sonje12@gmail.com>
2021-07-27 17:01:43 +0530

    Add project files.

---
+++ reading_csv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace reading_csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
